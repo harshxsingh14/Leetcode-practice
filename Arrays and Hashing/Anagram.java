84b1d4d128d9d97401fca2e76f68c10c8662410f
harshxsingh14 <harsh.singh1403@gmail.com>
2026-01-03 23:16:48 +0530

    Anagram array prgm

---
+++ Arrays and Hashing/Anagram.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Anagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] sSort = s.toCharArray();
+        char[] tSort = t.toCharArray();
+        Arrays.sort(sSort);
+        Arrays.sort(tSort);
+        return Arrays.equals(sSort, tSort);
+    }
+}
