51c92f6d934d86a633c4370144e0460f36c8ae45
harshxsingh14 <harsh.singh1403@gmail.com>
2026-01-06 23:08:14 +0530

    remove elements prgm

---
+++ Arrays and Hashing/remove_elements.java
@@ -0,0 +1,15 @@
+class remove_element {
+    public int removeElement(int[] nums, int val) {
+        int i = 0, n = nums.length;
+        while(i < n){
+            if (nums[i] == val){
+                nums[i] = nums[--n];
+            }
+            else{
+                i++;
+            }
+        }
+        return n;
+        
+    }
+}
