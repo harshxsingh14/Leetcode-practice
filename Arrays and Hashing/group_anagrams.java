96e123cc60d746206dd602021df7323211e58fcd
harshxsingh14 <harsh.singh1403@gmail.com>
2026-01-06 23:10:04 +0530

    group anagram  elements prgm

--- Arrays and Hashing/group_anagrams.java
+++ Arrays and Hashing/group_anagrams.java
@@ -1,13 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class group_anagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> res = new HashMap<>();
+
         for (String s : strs) {
             char[] charArray = s.toCharArray();
             Arrays.sort(charArray);
             String sortedS = new String(charArray);
+
             res.putIfAbsent(sortedS, new ArrayList<>());
             res.get(sortedS).add(s);
         }
+
         return new ArrayList<>(res.values());
     }
 }
